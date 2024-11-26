76ca05c8eceb606c67270533b9f11fbea0fd2f67
LarraSanti <santiagolarraburu4@gmail.com>
2024-11-25 23:30:07 -0300

    Levels and checkpoints

--- Assets/Scripts/FruitManager.cs
+++ Assets/Scripts/FruitManager.cs
@@ -1,14 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class FruitManager : MonoBehaviour
 {
+    public void Update()
+    {
+        AllFruitsCollected();
+    }
+
+
     public void AllFruitsCollected()
     {
-        if (transform.childCount == 1)
+        if (transform.childCount == 0)
         {
             Debug.Log("No quedan frutas. Ganaste!");
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex+1);
         }
     }
 }
